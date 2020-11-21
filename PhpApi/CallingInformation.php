bff67f3714efd17a04adca9b8f404ca32aa9356a
JacobRothDevelopment <rothy784715+github@gmail.com>
2020-11-21 18:02:16 -0500

    init stuff

--- PhpApi/CallingInformation.php
+++ PhpApi/CallingInformation.php
@@ -4,10 +4,17 @@ namespace PhpApi;
 
 class CallingInformation
 {
-    public string $Controller;
+    public ?string $Controller;
     public ?string $Action;
     /** @var mixed[string] $Args */
     // key = variable name
     // value = value
     public ?array $Args;
+
+    public function __construct()
+    {
+        $this->Controller = null;
+        $this->Action = null;
+        $this->Args = null;
+    }
 }
