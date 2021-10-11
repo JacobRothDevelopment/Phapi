8be4250959863f7960e48aac6a2a9b891290c617
JacobRothDevelopment <rothy784715+github@gmail.com>
2021-10-10 23:03:07 -0400

    reorganizing

---
+++ Example/loader.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . "/../src/ApiException.php";
+require_once __DIR__ . "/../src/HttpCode.php";
+require_once __DIR__ . "/../src/HttpMethod.php";
+require_once __DIR__ . "/../src/CallingInformation.php";
+require_once __DIR__ . "/../src/RouteVariable.php";
+require_once __DIR__ . "/../src/Route.php";
+require_once __DIR__ . "/../src/Routes.php";
+require_once __DIR__ . "/../src/Startup.php";
+require_once __DIR__ . "/../src/Controller.php";
+
+require_once __DIR__ . "/Classes/Item.php";
+require_once __DIR__ . "/Controllers/DefaultController.php";
+require_once __DIR__ . "/Controllers/SpecialController.php";
+require_once __DIR__ . "/Requests/IdRequest.php";
--- loader.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . "/Phapi/loader.php";
-
-require_once __DIR__ . "/Classes/Item.php";
-require_once __DIR__ . "/Controllers/DefaultController.php";
-require_once __DIR__ . "/Controllers/SpecialController.php";
-require_once __DIR__ . "/Requests/IdRequest.php";
