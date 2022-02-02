677ec62a3398aa6d7c6ae08611ca75ed0a7ee704
Jacob Roth <jacob@redhawk-tech.com>
2022-02-02 01:38:41 -0500

    actually providing a viable example

--- Example/index.php
+++ Example/index.php
@@ -10,34 +10,34 @@ use Phapi\Startup;
 $Routes = new Routes();
 $Routes->Add(new Route(
     "Update By Id",
-    "/api/v1/{controller}/{action}/{?id}",
+    "/Example/api/v1/{controller}/{action}/{?id}",
     [HttpMethod::Put, HttpMethod::Get],
     // NOTE: ^ this specifies that this route is only for PUT actions
     "Controllers"
 ));
 $Routes->Add(new Route(
     "Get By Id",
-    "/api/v2/{controller}/{id}",
+    "/Example/api/v2/{controller}/{id}",
     [HttpMethod::Get],
     "Controllers"
 ));
 $Routes->Add(new Route(
     "Double Id Get",
-    "/api/v1/{controller}/{action}/{id}/{?string}",
+    "/Example/api/v1/{controller}/{action}/{id}/{?string}",
     // ^ NOTE: id is a required input, string is optional
     [HttpMethod::Get],
     "Controllers"
 ));
 $Routes->Add(new Route(
     "Default",
-    "/api/v1/{controller}/{?action}",
+    "/Example/api/v1/{controller}/{?action}",
     // NOTE: this is how you'll call a method by http method
     null,
     "Controllers"
 ));
 $Routes->Add(new Route(
     "Calling Methods with Special URLs",
-    "/{id}",
+    "/Example/{id}",
     null,
     "V2",
     "Special",
